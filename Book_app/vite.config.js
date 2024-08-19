356288824069f068da7f6051559a70ef30d6d8e0
okemirio <okemirio@gmail.com>
2024-08-19 19:54:56 +0100

    just

--- Book_app/vite.config.js
+++ Book_app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    port: 'https://bookkapp-backend.vercel.app', // specify the port here
+    port: '3000', // specify the port here
   },
   plugins: [react()],
   build: {
