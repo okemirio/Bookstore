82b050ac80bfaa44b9951c77dfb0977989740e77
okemirio <okemirio@gmail.com>
2024-08-19 20:22:38 +0100

    hug

--- Book_app/src/Componets/Home.jsx
+++ Book_app/src/Componets/Home.jsx
@@ -1,41 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../Sharedcomponets/Header";
-import ListedProducts from "../Sharedcomponets/ListedProducts"; // Ensure ListedProducts is a default export
+import ListedProducts from "../Sharedcomponets/ListedProducts";
 import Footer from "../Sharedcomponets/Footer";
 import Image from '../../public/images/home-bg2.jpg';
 import Image7 from "../../public/images/about-img.jpg";
 
 const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
       {/* Hero Section */}
-      <div className="relative h-[400px] bg-cover bg-center" style={{ backgroundImage: `url(${Image})` }}>
+      <div className="relative h-[400px] sm:h-[300px] bg-cover bg-center" style={{ backgroundImage: `url(${Image})` }}>
         <div className="absolute inset-0 bg-black opacity-50"></div> {/* Dark overlay */}
-        <div className="relative z-10 flex flex-col m-20 items-center justify-center h-full text-white font-bold text-5xl text-center gap-5">
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white font-bold text-4xl sm:text-2xl text-center gap-3 px-4">
           <h1>HAND PICKED BOOK</h1>
           <h1>TO YOUR DOOR</h1>
-          <p className='font-thin text-sm'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br /> 
-            Nihil omnis iure consequuntur vel suscipit, voluptatum eum error excepturi dicta laudantium.
+          <p className='font-thin text-sm sm:text-xs'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br /> Nihil omnis iure consequuntur vel suscipit, voluptatum eum error excepturi dicta laudantium.
           </p>
-          <button className='text-sm text-white bg-blue-600 p-2 rounded-[4px]'>Discover More</button>
+          <button className='text-sm sm:text-xs text-white bg-blue-600 p-2 rounded-[4px]'>
+            Discover More
+          </button>
         </div>
       </div>
 
       {/* Main Content */}
       <Header />
-      <ListedProducts />
+      <div className="flex-grow">
+        <ListedProducts />
+      </div>
 
       {/* About Section */}
-      <div className="flex mx-10 p-8">
-        <div className="mt-10">
-          <img className="w-full h-full" src={Image7} alt="About Us" />
+      <div className="flex flex-col lg:flex-row mx-4 lg:mx-10 p-8 gap-6">
+        <div className="flex-1">
+          <img className="w-full h-auto object-cover" src={Image7} alt="About Us" />
         </div>
-        <div className="block text-center justify-center m-20 bg-slate-300 p-8 mx-0 ">
-          <h1 className="font-extrabold">ABOUT US</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, accusantium!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, alias?</p>
+        <div className="flex-1 flex flex-col items-center justify-center bg-slate-300 p-6 text-center">
+          <h1 className="font-extrabold text-2xl sm:text-xl">ABOUT US</h1>
+          <p className="mt-4 text-sm sm:text-xs">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, accusantium!
+          </p>
+          <p className="mt-2 text-sm sm:text-xs">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, alias?
+          </p>
           <Link to="/about" className="bg-blue-600 text-white p-2 rounded-md mt-8 inline-block text-center">
             Read More
           </Link>
@@ -43,16 +51,14 @@ const Home = () => {
       </div>
 
       {/* Contact Section */}
-      <div className="bg-black text-white text-center p-5 ">
-        <h1 className="font-bold">HAVE ANY QUESTIONS?</h1>
-        <div className="font-thin">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            <br /> In eveniet dolore ea fugiat illo at porro iure rem.
-            Inventore, exercitationem.
-          </p>
-        </div>
-        <button className="bg-blue-600 text-white p-2 rounded-md m-5">
+      <div className="bg-black text-white text-center p-5 flex flex-col items-center gap-3">
+        <h1 className="font-bold text-lg sm:text-md">HAVE ANY QUESTIONS?</h1>
+        <p className="font-thin text-sm sm:text-xs px-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <br /> In eveniet dolore ea fugiat illo at porro iure rem.
+          Inventore, exercitationem.
+        </p>
+        <button className="bg-blue-600 text-white p-2 rounded-md">
           Contact us
         </button>
       </div>
--- Book_app/src/Sharedcomponets/Footer.jsx
+++ Book_app/src/Sharedcomponets/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
 import { FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
@@ -8,115 +8,113 @@ import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 
-
-
 const Footer = () => {
   return (
-   <div className='bg-white'>
-     <div className='flex my-6 items-center justify-evenly'>
-        <div className='list-none'>
-            <div className='py-4'>
-            <h1 className='font-bold '>Quick Links</h1>
-            </div>
-            <nav>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                <Link to="/about">About</Link>
-                </li>
-                <li>
-                <Link to="/shop">Shop</Link>
-
-                </li>
-                <li>
-                <Link to="/contact">Contact</Link>
-
-                </li>
-            </nav>
-        </div>
-        <div className='list-none'>
-            <div className='py-4'>
-            <h1 className='font-bold '>EXTRA LINKS</h1>
-            </div>
-            <nav>
-                <li>
-                <Link to="/">login</Link>
-
-                </li>
-                <li>
-                <Link to="/register">register</Link>
-                </li>
-                <li>
-                <Link to="/orders">Cart</Link>
-
-                </li>
-                <li>
-                    <Link to="/orders">Order</Link>
-                </li>
-            </nav>
-        </div>
-        <div>
+    <div className='bg-white'>
+      <div className='flex flex-wrap justify-around my-6 items-start'>
+        <div className='list-none flex-1 min-w-[150px] px-4'>
           <div className='py-4'>
-          <h1 className='font-bold'>CONTACT INFO</h1>
+            <h1 className='font-bold '>Quick Links</h1>
           </div>
-            <nav>
-                <li className='flex gap-2'>
-                <FaPhone />
-                09018922099
-                </li>
-                <li className='flex gap-2'>
-                <FaPhone />
-                09018922099
-                </li>
-                <li className='flex gap-2'>
-                <MdEmail />
-                johnbosco@gmail.com
-                </li>
-                <li className='flex gap-2'>
-                <CiLocationOn />
-                12 johnny avenue yaba
-                </li>
-            </nav>
+          <nav>
+            <li className='py-1'>
+              <Link to="/home">Home</Link>
+            </li>
+            <li className='py-1'>
+              <Link to="/about">About</Link>
+            </li>
+            <li className='py-1'>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li className='py-1'>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </nav>
         </div>
-        <div className=' list-none'>
-            <div>
-            <h1 className='font-bold py-4'> FOLLOW US</h1>
-            </div>
-            <nav className='cursor-pointer'>
-                <li>
-                    <a className='flex gap-2' href="https://web.facebook.com/">
-                    <FaFacebookF />
-                    Facebook
-                    </a>
-                
-                </li>
-                <li>
-                 <a className='flex gap-2' href="https://x.com/">
-                 <FaTwitter />
-                 twitter
-                 </a>
-                </li>
-                <li>
-                <a className='flex gap-2' href="https://www.instagram.com/">
+
+        <div className='list-none flex-1 min-w-[150px] px-4'>
+          <div className='py-4'>
+            <h1 className='font-bold '>EXTRA LINKS</h1>
+          </div>
+          <nav>
+            <li className='py-1'>
+              <Link to="/">Login</Link>
+            </li>
+            <li className='py-1'>
+              <Link to="/register">Register</Link>
+            </li>
+            <li className='py-1'>
+              <Link to="/orders">Cart</Link>
+            </li>
+            <li className='py-1'>
+              <Link to="/orders">Order</Link>
+            </li>
+          </nav>
+        </div>
+
+        <div className='flex-1 min-w-[200px] px-4'>
+          <div className='py-4'>
+            <h1 className='font-bold'>CONTACT INFO</h1>
+          </div>
+          <nav>
+            <li className='flex items-center gap-2 py-1'>
+              <FaPhone />
+              09018922099
+            </li>
+            <li className='flex items-center gap-2 py-1'>
+              <FaPhone />
+              09018922099
+            </li>
+            <li className='flex items-center gap-2 py-1'>
+              <MdEmail />
+              johnbosco@gmail.com
+            </li>
+            <li className='flex items-center gap-2 py-1'>
+              <CiLocationOn />
+              12 Johnny Avenue, Yaba
+            </li>
+          </nav>
+        </div>
+
+        <div className='list-none flex-1 min-w-[150px] px-4'>
+          <div className='py-4'>
+            <h1 className='font-bold'>FOLLOW US</h1>
+          </div>
+          <nav>
+            <li className='py-1'>
+              <a className='flex items-center gap-2' href="https://web.facebook.com/">
+                <FaFacebookF />
+                Facebook
+              </a>
+            </li>
+            <li className='py-1'>
+              <a className='flex items-center gap-2' href="https://x.com/">
+                <FaTwitter />
+                Twitter
+              </a>
+            </li>
+            <li className='py-1'>
+              <a className='flex items-center gap-2' href="https://www.instagram.com/">
                 <FaInstagram />
-                instagram
-                </a>
-                </li>
-                <li>
-                    <a className='flex gap-2' href="https://www.linkedin.com"> <FaLinkedin />
-                    linkedin</a>
-                </li>
-            </nav>
+                Instagram
+              </a>
+            </li>
+            <li className='py-1'>
+              <a className='flex items-center gap-2' href="https://www.linkedin.com">
+                <FaLinkedin />
+                LinkedIn
+              </a>
+            </li>
+          </nav>
         </div>
-        <div>  
-        </div>
-        
-    </div>
-    <div className="container mx-auto mt-8 text-center border-t border-gray-700 pt-4">
-        <p className="text-sm">@copyright; &copy; {new Date().getFullYear()} Mr Okemiri Onyedikachi. All rights reserved.</p>
       </div>
-   </div>
-  )
+      <div className="text-center border-t border-gray-300 pt-4">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Mr Okemiri Onyedikachi. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 }
+
 export default Footer;
